5b48a41a2b6942701ae312ebbee7c43f848c9597
Nelly Sugu <jonly03@gmail.com>
2023-06-22 09:55:50 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from Express</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`web server listening on PORT: ${PORT}`);
+});
